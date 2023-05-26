7f664a0c9801a0d2f2657ba90ad09274f3cc28fc
victor lebret <lebret.victor02@gmail.com>
2023-05-26 16:10:11 +0200

    hello victor

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+def new
+  @list = List.find(params[:list_id])
+  @bookmark = Bookmark.new
+end
+
+def create
+  @list = List.find(params[:list_id])
+  @movie = Movie.find(params[:bookmark][:movie_id])
+
+  @bookmark = Bookmark.new(bookmark_params)
+  @bookmark.list = @list
+  @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmark_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show"
+
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new"
+  # post "lists/:id/bookmarks", to: "bookmarks#create"
+  # delete "bookmarks/:id", to: "bookmarks#destroy"
+  root to: 'lists#index'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+
+  end
+  resources :bookmarks, only: [:destroy]
 end
